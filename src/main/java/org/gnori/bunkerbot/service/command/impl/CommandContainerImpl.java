4c567136a6a38b3bb13e73e7f6982c08482f3256
gnori-zon <konasenkos@gmail.com>
2023-12-17 00:44:32 +0300

    #1 #refactor #service - command container
    refactor MessageTypeCommandContainer, add MessageTypeCommandContainer

--- src/main/java/org/gnori/bunkerbot/service/command/impl/CommandContainerImpl.java
+++ src/main/java/org/gnori/bunkerbot/service/command/impl/CommandContainerImpl.java
@@ -5,6 +5,7 @@ import lombok.NonNull;
 import lombok.experimental.FieldDefaults;
 import org.gnori.bunkerbot.service.command.Command;
 import org.gnori.bunkerbot.service.command.CommandContainer;
+import org.gnori.bunkerbot.service.command.impl.text.TextCommand;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -16,16 +17,17 @@ import java.util.stream.Collectors;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class CommandContainerImpl implements CommandContainer<MessageType, Command<MessageType>> {
 
-    Map<MessageType, Command<MessageType>> commands;
+    Map<MessageType, MessageTypeCommandContainer> containers;
 
-    public CommandContainerImpl(List<Command<MessageType>> commands) {
+    public CommandContainerImpl(List<MessageTypeCommandContainer> containers) {
 
-        this.commands = commands.stream()
-                .collect(Collectors.toMap(Command::getSupportedKey, Function.identity()));
+        this.containers = containers.stream()
+                .collect(Collectors.toMap(MessageTypeCommandContainer::getSupportedKey, Function.identity()));
     }
 
     @Override
     public @NonNull Command<MessageType> retrieveCommand(MessageType key) {
-        return commands.get(key);
+
+        return containers.get(key);
     }
 }
