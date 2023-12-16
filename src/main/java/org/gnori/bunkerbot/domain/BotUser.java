0799ba5eba297c8ab94b40a95d14619d91e7410e
gnori-zon <konasenkos@gmail.com>
2023-12-17 00:36:05 +0300

    #1 #add #domain - BotUserState
    add migration

--- src/main/java/org/gnori/bunkerbot/domain/BotUser.java
+++ src/main/java/org/gnori/bunkerbot/domain/BotUser.java
@@ -1,25 +1,36 @@
 package org.gnori.bunkerbot.domain;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 import org.gnori.bunkerbot.domain.abstracts.AuditableEntity;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 
 import static org.gnori.bunkerbot.domain.BotUser.BOT_USER_TABLE;
+import static org.gnori.bunkerbot.domain.constants.DefaultGenericGenerator.*;
 
+@GenericGenerator(
+        name = NAME,
+        strategy = STRATEGY,
+        parameters = {
+                @Parameter(name = Params.SEQUENCE_NAME, value = BOT_USER_TABLE + POSTGRES_ENTITY_SEQUENCE_SUFFIX),
+                @Parameter(name = Params.INITIAL_VALUE, value = Params.DEFAULT_VALUE),
+                @Parameter(name = Params.INCREMENT_SIZE, value = Params.DEFAULT_VALUE)
+        }
+)
 @Getter
 @Setter
+@Builder
 @Entity
 @Table(name = BOT_USER_TABLE)
-@NoArgsConstructor
 @AllArgsConstructor
+@RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PROTECTED)
 public class BotUser extends AuditableEntity {
 
-    static final String BOT_USER_TABLE = "bot_user";
+    static final String BOT_USER_TABLE = "bot_users";
 
     @Column(name = "chat_id", nullable = false, unique = true)
     Long chatId;
@@ -27,9 +38,15 @@ public class BotUser extends AuditableEntity {
     @Column(name = "username")
     String username;
 
+    @Builder.Default
     @Column(name = "is_admin", nullable = false)
-    Boolean isAdmin = false;
+    boolean isAdmin = false;
 
-    @Column(name = "is_blocked", nullable = false)
-    Boolean isBlocked = false;
+    @Builder.Default
+    @Column(name = "is_active", nullable = false)
+    boolean isActive = false;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "state")
+    BotUserState state;
 }
--- src/main/resources/db/changelog/changesets/V1702712371_create_user_table.sql
+++ src/main/resources/db/changelog/changesets/V1702712371_create_user_table.sql
@@ -1,9 +1,12 @@
 -- liquibase formatted sql
 -- changeset gnori-zon:V1702712371_create_user_table
 
-CREATE TABLE bot_user (
-    chat_id    BIGSERIAL PRIMARY KEY,
+CREATE TABLE bot_users (
+    id         BIGSERIAL PRIMARY KEY,
+    chat_id    BIGINT UNIQUE,
     username   VARCHAR,
-    is_admin   BOOLEAN NOT NULL DEFAULT false,
-    is_blocked BOOLEAN NOT NULL DEFAULT false
+    is_admin   BOOLEAN                  NOT NULL DEFAULT false,
+    is_active  BOOLEAN                  NOT NULL DEFAULT false,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP WITH TIME ZONE
 )
